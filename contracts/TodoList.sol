998e149fb1fe4b48bd0be99f45755a10a7361b72
funnydev2512 <sherlock2512@gmail.com>
2022-09-26 14:28:58 +0700

    Update files

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,21 @@ pragma solidity ^0.5.0;
 
 contract TodoList{ 
   uint public taskCount = 0;
+
+  struct Task{
+    uint id;
+    string context;
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+
+  constructor() public{
+    createTask("Check out TommyXiaomi");
+  }
+
+  function createTask(string memory _context) public{
+    taskCount++;
+    tasks[taskCount] = Task(taskCount, _context, false);
+  }
 } 
